fd06e2e0e6d65637bea496786a95b3be6b41656a
totemancer <josip@totemancer.com>
2024-05-19 01:56:44 +0200

    Fully working V5R1 Integration
    Synced with dev-v19 branch from xssnick/tonutils-go upstream.

--- ton/wallet/v5r1.go
+++ ton/wallet/v5r1.go
@@ -2,204 +2,91 @@ package wallet
 
 import (
 	"context"
-	"encoding/binary"
 	"errors"
 	"fmt"
-	"math/big"
+	"github.com/xssnick/tonutils-go/tlb"
+	"github.com/xssnick/tonutils-go/ton"
 	"time"
 
-	"github.com/xssnick/tonutils-go/tlb"
 	"github.com/xssnick/tonutils-go/tvm/cell"
 )
 
-// Verified V5 Contract from:
 // https://github.com/tonkeeper/tonkeeper-ton/commit/e8a7f3415e241daf4ac723f273fbc12776663c49#diff-c20d462b2e1ec616bbba2db39acc7a6c61edc3d5e768f5c2034a80169b1a56caR29
 const _V5R1CodeHex = "b5ee9c7241010101002300084202e4cf3b2f4c6d6a61ea0f2b5447d266785b26af3637db2deee6bcd1aa826f34120dcd8e11"
 
-// Constants
-const (
-	authSignedExternal  = 0x7369676e
-	authSignedInternal  = 0x73696e74
-	maxMessages         = 255
-	maxActionListLength = 10000 // Change later to correct one
-)
-
-type KWalletId struct {
-	WalletVersion   uint8
-	NetworkGlobalId int32
-	WorkChain       int8
-	SubwalletNumber uint32
+type ConfigV5R1 struct {
+	NetworkGlobalID int32
+	Workchain       int8
 }
 
-type ConfigWalletV5 struct {
-	MessageTTL     uint32
-	MessageBuilder func(ctx context.Context, subWalletID uint32) (id uint32, createdAt int64, err error)
-}
-
-type SpecWalletV5 struct {
-	wallet *Wallet
-	config ConfigWalletV5
+type SpecV5R1 struct {
+	SpecRegular
 	SpecSeqno
+
+	config ConfigV5R1
 }
 
-func bufferToBigInt(buffer []byte) *big.Int {
-	return new(big.Int).SetBytes(buffer)
-}
+const MainnetGlobalID = -239
+const TestnetGlobalID = -3
 
-// Convert a byte buffer to a uint64
-func bufferToUint64(buffer []byte) uint64 {
-	if len(buffer) < 8 {
-		// If buffer is smaller than 8 bytes, pad with zeros
-		paddedBuffer := make([]byte, 8)
-		copy(paddedBuffer[8-len(buffer):], buffer)
-		return binary.BigEndian.Uint64(paddedBuffer)
-	}
-	return binary.BigEndian.Uint64(buffer[:8])
-}
+func (s *SpecV5R1) BuildMessage(ctx context.Context, _ bool, _ *ton.BlockIDExt, messages []*Message) (_ *cell.Cell, err error) {
+	// TODO: remove block, now it is here for backwards compatibility
 
-func (s *SpecWalletV5) BuildMessage(ctx context.Context, messages []*Message) (*cell.Cell, error) {
-	// Define network
-	walletId := KWalletId{
-		WalletVersion:   0,
-		NetworkGlobalId: -3,
-		WorkChain:       0,
-		SubwalletNumber: 0,
+	if len(messages) > 255 {
+		return nil, errors.New("for this type of wallet max 4 messages can be sent in the same time")
 	}
 
-	if s.config.MessageBuilder == nil {
-		return nil, errors.New("query fetcher is not defined in spec config")
-	}
-	if s.config.MessageTTL >= 1<<22 {
-		return nil, fmt.Errorf("too long ttl")
-	}
-	if s.config.MessageTTL <= 5 {
-		return nil, fmt.Errorf("too short ttl")
-	}
-
-	queryID, createdAt, err := s.config.MessageBuilder(ctx, walletId.SubwalletNumber)
-	if err != nil {
-		return nil, fmt.Errorf("failed to fetch query id: %w", err)
-	}
-	if queryID >= 1<<23 {
-		return nil, fmt.Errorf("too big query id")
-	}
-	if createdAt <= 0 {
-		return nil, fmt.Errorf("created at should be positive")
-	}
-
-	seq, err := s.seqnoFetcher(ctx, walletId.SubwalletNumber)
+	seq, err := s.seqnoFetcher(ctx, s.wallet.subwallet)
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch seqno: %w", err)
 	}
 
-	if len(messages) > maxMessages {
-		return nil, errors.New("for this type of wallet max 255 messages can be sent at the same time")
-	} else if len(messages) == 0 {
-		return nil, errors.New("should have at least one message")
-	}
-
-	var msg *Message
-	msg, err = s.packActions(uint64(seq), messages)
+	actions, err := packV5Actions(messages)
 	if err != nil {
-		return nil, fmt.Errorf("failed to pack messages: %w", err)
+		return nil, fmt.Errorf("failed to build actions: %w", err)
 	}
 
-	msgCell, err := tlb.ToCell(msg.InternalMessage)
-	if err != nil {
-		return nil, fmt.Errorf("failed to convert message to cell: %w", err)
-	}
+	payload := cell.BeginCell().
+		MustStoreUInt(0x7369676e, 32). // external sign op code
+		MustStoreInt(int64(s.config.NetworkGlobalID), 32).
+		MustStoreInt(int64(s.config.Workchain), 8).
+		MustStoreUInt(0, 8). // version of v5
+		MustStoreUInt(uint64(s.wallet.subwallet), 32).
+		MustStoreUInt(uint64(timeNow().Add(time.Duration(s.messagesTTL)*time.Second).UTC().Unix()), 32).
+		MustStoreUInt(uint64(seq), 32).
+		MustStoreBuilder(actions)
 
-	walletIdCell := cell.BeginCell().
-		MustStoreInt(int64(walletId.NetworkGlobalId), 32).
-		MustStoreInt(int64(walletId.WorkChain), 8).
-		MustStoreUInt(uint64(walletId.WalletVersion), 8).
-		MustStoreUInt(uint64(walletId.SubwalletNumber), 32). // 0??? get_wallet_id 0xffffff11000000000000?
-		EndCell()
+	sign := payload.EndCell().Sign(s.wallet.key)
+	msg := cell.BeginCell().MustStoreBuilder(payload).MustStoreSlice(sign, 512).EndCell()
 
-	// Convert the cell to a byte buffer
-	buffer := walletIdCell.ToBOC()
-
-	// Log the signature
-	fmt.Printf("NetworkGlobalId: %d (size: %d bits)\n", walletId.NetworkGlobalId, 32)
-	fmt.Printf("WorkChain: %d (size: %d bits)\n", walletId.WorkChain, 8)
-	fmt.Printf("WalletVersion: %d (size: %d bits)\n", walletId.WalletVersion, 8)
-	fmt.Printf("SubwalletNumber: %d (size: %d bits)\n", walletId.SubwalletNumber, 32)
-	fmt.Printf("TTL: %d (size: %d bits)\n", uint64(time.Now().Add(time.Duration(s.config.MessageTTL)*time.Second).UTC().Unix()), 32)
-	fmt.Printf("Sequence Number: %d (size: %d bits)\n", uint64(seq), 32)
-	fmt.Printf("Query ID: %d (size: %d bits)\n", uint64(queryID), 64)
-	//fmt.Printf("Signature: %x\n", signature)
-	//fmt.Printf("Payload size: %d bits\n", len(payload.ToBOC())*8)
-	//fmt.Printf("Signature size: %d bits\n", len(signature)*8)
-
-	signedPayloadTMP := cell.BeginCell().
-		MustStoreUInt(authSignedExternal, 32).     // Ensure opcode alignment
-		MustStoreUInt(bufferToUint64(buffer), 80). // Store the byte buffer directly as a bit slice
-		MustStoreUInt(uint64(time.Now().Add(time.Duration(s.config.MessageTTL)*time.Second).UTC().Unix()), 32).
-		MustStoreUInt(uint64(seq), 32). // Ensure sequence number is stored as 32 bits
-		MustStoreRef(msgCell).
-		EndCell()
-
-	// Construct the final signed payload
-	signedPayload := cell.BeginCell().
-		MustStoreUInt(authSignedExternal, 32).     // Ensure opcode alignment
-		MustStoreUInt(bufferToUint64(buffer), 80). // Store the byte buffer directly as a bit slice
-		MustStoreUInt(uint64(time.Now().Add(time.Duration(s.config.MessageTTL)*time.Second).UTC().Unix()), 32).
-		MustStoreUInt(uint64(seq), 32). // Ensure sequence number is stored as 32 bits
-		MustStoreRef(msgCell).
-		MustStoreSlice(signedPayloadTMP.Sign(s.wallet.key), 512). // need to sign the whole cell and add before the end
-		EndCell()
-
-	return signedPayload, nil
+	return msg, nil
 }
 
-// packActions method to pack multiple actions into a single message
-func (s *SpecWalletV5) packActions(queryId uint64, messages []*Message) (*Message, error) {
-	fmt.Printf("I got called !!!")
-	amt := big.NewInt(0)
-	listBuilder := cell.BeginCell().MustStoreUInt(0, 1)
+func packV5Actions(messages []*Message) (*cell.Builder, error) {
+	if len(messages) > 255 {
+		return nil, fmt.Errorf("max 255 messages allowed for v5")
+	}
 
+	var list = cell.BeginCell().EndCell()
 	for _, message := range messages {
-		amt = amt.Add(amt, message.InternalMessage.Amount.Nano())
-
 		outMsg, err := tlb.ToCell(message.InternalMessage)
 		if err != nil {
 			return nil, err
 		}
 
-		msg := cell.BeginCell().
-			MustStoreUInt(0x0ec3c86d, 32). // action_send_msg opcode
+		/*
+			out_list_empty$_ = OutList 0;
+			out_list$_ {n:#} prev:^(OutList n) action:OutAction
+			  = OutList (n + 1);
+			action_send_msg#0ec3c86d mode:(## 8)
+			  out_msg:^(MessageRelaxed Any) = OutAction;
+		*/
+		msg := cell.BeginCell().MustStoreUInt(0x0ec3c86d, 32).
 			MustStoreUInt(uint64(message.Mode), 8).
-			MustStoreRef(cell.BeginCell().MustStoreRef(outMsg).EndCell()).
-			EndCell()
+			MustStoreRef(outMsg)
 
-		listBuilder.MustStoreRef(
-			cell.BeginCell().MustStoreRef(msg).EndCell(),
-		)
+		list = cell.BeginCell().MustStoreRef(list).MustStoreBuilder(msg).EndCell()
 	}
 
-	list := listBuilder.EndCell()
-
-	// Validate the action list length
-	if len(list.ToBOC()) > maxActionListLength {
-		return nil, errors.New("action list too long")
-	}
-
-	internalMessageCell := cell.BeginCell().
-		MustStoreUInt(authSignedInternal, 32). // internal_signed opcode
-		MustStoreUInt(queryId, 64).
-		MustStoreRef(list).
-		EndCell()
-
-	message := &Message{
-		Mode: 1 + 2,
-		InternalMessage: &tlb.InternalMessage{
-			IHRDisabled: true,
-			Bounce:      false,
-			DstAddr:     s.wallet.addr,
-			Amount:      tlb.FromNanoTON(amt),
-			Body:        internalMessageCell,
-		},
-	}
-
-	return message, nil
+	return cell.BeginCell().MustStoreUInt(0, 1).MustStoreRef(list), nil
 }
--- ton/wallet/wallet.go
+++ ton/wallet/wallet.go
@@ -37,8 +37,7 @@ const (
 	V3                         = V3R2
 	V4R1               Version = 41
 	V4R2               Version = 42
-	V5R1_Testnet       Version = 50
-	V5R1_Mainnet       Version = 51
+	V5R1               Version = 51
 	HighloadV2R2       Version = 122
 	HighloadV2Verified Version = 123
 	HighloadV3         Version = 300
@@ -73,7 +72,7 @@ var (
 		V2R1: _V2R1CodeHex, V2R2: _V2R2CodeHex,
 		V3R1: _V3R1CodeHex, V3R2: _V3R2CodeHex,
 		V4R1: _V4R1CodeHex, V4R2: _V4R2CodeHex,
-		V5R1_Testnet: _V5R1CodeHex, V5R1_Mainnet: _V5R1CodeHex,
+		V5R1:         _V5R1CodeHex,
 		HighloadV2R2: _HighloadV2R2CodeHex, HighloadV2Verified: _HighloadV2VerifiedCodeHex,
 		HighloadV3: _HighloadV3CodeHex,
 		Lockup:     _LockupCodeHex,
@@ -150,6 +149,11 @@ func FromPrivateKey(api TonAPI, key ed25519.PrivateKey, version VersionConfig) (
 		return nil, err
 	}
 
+	// Default SubWallet 0 only for V5R1 else 698983191
+	if version == V5R1 {
+		defaultSubwallet = 0
+	}
+
 	w := &Wallet{
 		api:       api,
 		key:       key,
@@ -168,7 +172,7 @@ func FromPrivateKey(api TonAPI, key ed25519.PrivateKey, version VersionConfig) (
 
 func getSpec(w *Wallet) (any, error) {
 	switch v := w.ver.(type) {
-	case Version:
+	case Version, ConfigV5R1:
 		regular := SpecRegular{
 			wallet:      w,
 			messagesTTL: 60 * 3, // default ttl 3 min
@@ -204,36 +208,17 @@ func getSpec(w *Wallet) (any, error) {
 			return &SpecHighloadV2R2{regular, SpecQuery{}}, nil
 		case HighloadV3:
 			return nil, fmt.Errorf("use ConfigHighloadV3 for highload v3 spec")
-		case V5R1_Testnet, V5R1_Mainnet:
-			return nil, fmt.Errorf("use ConfigWalletV5 for WalletV5 spec")
-		}
-	case ConfigWalletV5:
-		seqnoFetcher := func(ctx context.Context, subWallet uint32) (uint32, error) {
-			block, err := w.api.CurrentMasterchainInfo(ctx)
-			if err != nil {
-				return 0, fmt.Errorf("failed to get block: %w", err)
-			}
-
-			resp, err := w.api.WaitForBlock(block.SeqNo).RunGetMethod(ctx, block, w.addr, "seqno")
-			if err != nil {
-				if cErr, ok := err.(ton.ContractExecError); ok && cErr.Code == ton.ErrCodeContractNotInitialized {
-					return 0, nil
+		case V5R1:
+			return nil, fmt.Errorf("use ConfigV5R1 for v5 spec")
+		default:
+			switch x := w.ver.(type) {
+			case ConfigV5R1:
+				if x.NetworkGlobalID == 0 {
+					return nil, fmt.Errorf("NetworkGlobalID should be set in v5 config")
 				}
-				return 0, fmt.Errorf("get seqno err: %w", err)
+				return &SpecV5R1{SpecRegular: regular, SpecSeqno: SpecSeqno{seqnoFetcher: seqnoFetcher}, config: x}, nil
 			}
-
-			iSeq, err := resp.Int(0)
-			if err != nil {
-				return 0, fmt.Errorf("failed to parse seqno: %w", err)
-			}
-			return uint32(iSeq.Uint64()), nil
 		}
-
-		return &SpecWalletV5{
-			wallet:    w,
-			config:    v,
-			SpecSeqno: SpecSeqno{seqnoFetcher: seqnoFetcher}, // Specify the field name for SpecSeqno
-		}, nil
 	case ConfigHighloadV3:
 		return &SpecHighloadV3{wallet: w, config: v}, nil
 	}
@@ -333,9 +318,13 @@ func (w *Wallet) PrepareExternalMessageForMany(ctx context.Context, withStateIni
 
 	var msg *cell.Cell
 	switch v := w.ver.(type) {
-	case Version:
+	case Version, ConfigV5R1:
+		if _, ok := v.(ConfigV5R1); ok {
+			v = V5R1
+		}
+
 		switch v {
-		case V3R2, V3R1, V4R2, V4R1:
+		case V3R2, V3R1, V4R2, V4R1, V5R1:
 			msg, err = w.spec.(RegularBuilder).BuildMessage(ctx, !withStateInit, nil, messages)
 			if err != nil {
 				return nil, fmt.Errorf("build message err: %w", err)
@@ -352,11 +341,6 @@ func (w *Wallet) PrepareExternalMessageForMany(ctx context.Context, withStateIni
 		}
 	case ConfigHighloadV3:
 		msg, err = w.spec.(*SpecHighloadV3).BuildMessage(ctx, messages)
-		if err != nil {
-			return nil, fmt.Errorf("build message err: %w", err)
-		}
-	case ConfigWalletV5:
-		msg, err = w.spec.(*SpecWalletV5).BuildMessage(ctx, messages)
 		if err != nil {
 			return nil, fmt.Errorf("build message err: %w", err)
 		}
