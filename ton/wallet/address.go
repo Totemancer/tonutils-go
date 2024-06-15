85418336af6c83f8ca004e4f0a858017c8adcbb6
Totemancer <josip@totemancer.com>
2024-06-15 04:16:23 +0200

    Getting ready for Final W5 Contract

--- ton/wallet/address.go
+++ ton/wallet/address.go
@@ -88,7 +88,8 @@ func GetStateInit(pubKey ed25519.PublicKey, version VersionConfig, subWallet uin
 		config := version.(ConfigV5R1)
 
 		data = cell.BeginCell().
-			MustStoreUInt(0, 33). // seqno
+			MustStoreBoolBit(false). // signature_auth_disabled (https://github.com/Skydev0h/w5/commit/1f9b0a3a19edfab4ae4d6bfd23eb0fea1e5fc181#diff-09a46711e64512185158086f7e6ece30bda4f749d144fc64baa7e9d913b960cfR27)
+			MustStoreUInt(0, 32).    // seqno (W5 Beta 33 -> W5 Final 32)
 			MustStoreInt(int64(config.NetworkGlobalID), 32).
 			MustStoreInt(int64(config.Workchain), 8).
 			MustStoreUInt(0, 8). // version of v5
